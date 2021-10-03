05b2275d732494514ba3bd57b433f3345145c1f6
joy-rosie <joytolia@hotmail.com>
2021-10-03 10:50:52 +0100

    initial add

---
+++ peas/src/main/java/com/peas/App.java
@@ -0,0 +1,13 @@
+package com.peas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
